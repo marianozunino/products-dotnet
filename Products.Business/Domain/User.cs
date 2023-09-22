61af119383538aa8133f3e767722b6d947500ea0
Mariano Z. <marianoz@posteo.net>
2023-09-21 23:09:28 -0300

    feat: ef migrations with sqlite

---
+++ Products.Business/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Products.Common.Types;
+
+namespace Products.Business.Domain;
+
+public class User
+{
+    [Required] public required string Name { get; set; }
+    [Required] public Color Color { get; set; }
+    [Required] public required string Brand { get; set; }
+    [Required] public int Id { get; set; }
+}
